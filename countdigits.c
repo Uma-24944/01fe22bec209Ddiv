4cf88012e4506968764b695feea8f39b2f1052d9
Uma-24944 <119474542+Uma-24944@users.noreply.github.com>
2024-03-11 20:16:19 +0530

    Update countdigits.c

--- countdigits.c
+++ countdigits.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 int main() {
-    char c;
-    int lowercase_vowel, uppercase_vowel;
-    printf("Enter an alphabet: ");
-    scanf("%c", &c);
+  long long n;
+  int count = 0;
+  printf("Enter an integer: ");
+  scanf("%lld", &n);
+ 
+  // iterate at least once, then until n becomes 0
+  // remove last digit from n in each iteration
+  // increase count by 1 in each iteration
+  do {
+    n /= 10;
+    ++count;
+  } while (n != 0);
 
-    // evaluates to 1 if variable c is a lowercase vowel
-    lowercase_vowel = (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u');
-
-    // evaluates to 1 if variable c is a uppercase vowel
-    uppercase_vowel = (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U');
-
-    // evaluates to 1 (true) if c is a vowel
-    if (lowercase_vowel || uppercase_vowel)
-        printf("%c is a vowel.", c);
-    else
-        printf("%c is a consonant.", c);
-    return 0;
+  printf("Number of digits: %d", count);
 }
